70a63559d9aaef6fd52a9246eb49786eb38238c1
AndreyAA-code <otd53@yandex.ru>
2025-04-03 16:34:22 +0300

    добавил метод remove(taskId) в истории просмотра, при удалении задач и их наследников ушел домой с рабочего ПК

--- src/InMemoryHistoryManager.java
+++ src/InMemoryHistoryManager.java
@@ -1,14 +1,16 @@
-import java.util.List;
-import java.util.ArrayList;
+import java.util.*;
 
 public class InMemoryHistoryManager implements HistoryManager {
 
    // private Integer maxHistoryNumberlist = 10; //не нужно в спринте 6
    // private Integer historyNumberlist = 0; //не нужно в спринте 6
-    private List<Task> historyList = new ArrayList<>();
+    private List<Task> historyListForPrint = new ArrayList<>();
+    private Map<Integer, Node <Task>> nodesListLink= new HashMap<>();
+    private HistoryLinkedList<Task> historyList =new HistoryLinkedList<Task>();
 
     @Override
     public List<Task> getHistory() {
+
         for (int i = 0; i < historyList.size(); i++) { //хотя может надо просто вернуть ArrayList, а распечатывать его потом
             System.out.println("history" + i + ".   " + historyList.get(i));
         }
@@ -28,4 +30,25 @@ public class InMemoryHistoryManager implements HistoryManager {
     public void remove (int id) {
 
     }
+
+    public class HistoryLinkedList<Task> {
+        public Node<Task> head;
+        public Node<Task> tail;
+        private int size = 0;
+
+    }
+
+    class Node <Task> {
+        public Task data;
+        public Node<Task> next;
+        public Node<Task> prev;
+
+        public Node <Task data> {
+            this.data = data;
+            this.next = null;
+            this.prev= null;
+        }
+
+    }
+
 }
