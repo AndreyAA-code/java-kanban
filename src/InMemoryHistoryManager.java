1515e448d2712ef0e9a33d93e8ba262fafbf61ee
AndreyAA-code <otd53@yandex.ru>
2025-04-05 14:33:12 +0300

    сохраняю на ночь

--- src/InMemoryHistoryManager.java
+++ src/InMemoryHistoryManager.java
@@ -3,32 +3,23 @@ import java.util.*;
 public class InMemoryHistoryManager implements HistoryManager {
 
 
-
     public Node head;
     public Node tail;
     public List<Task> historyListForPrint; // для отдачи истории просмотров
 
     public static Map<Integer, Node<Task>> getNodesListLink() {
-        return nodesListLink;
+        return historyNodesListLink;
     }
 
-    public static Map<Integer, Node<Task>> nodesListLink; //для быстрого поиска нода для удаления
+    public static Map<Integer, Node<Task>> historyNodesListLink; //для быстрого поиска нода для удаления
 
     public InMemoryHistoryManager() {
-        nodesListLink = new HashMap<>();
+        historyNodesListLink = new HashMap<>();
     }
+
     @Override
     public List<Task> getHistory() {
-        historyListForPrint = new LinkedList<>();
-        Node<Task> currentNode = head;
-        while (currentNode != null) {
-            historyListForPrint.add(currentNode.data);
-            currentNode = currentNode.next;
-        }
-        for (Task task : historyListForPrint){
-            System.out.println(task);
-        }
-        return historyListForPrint;
+        return getTasks();
     }
 
     @Override
@@ -39,28 +30,47 @@ public class InMemoryHistoryManager implements HistoryManager {
     @Override
     public void remove(int id) {
 
+
     }
 
     public void linkLast(Task task) {
-       final Node<Task> oldTail = tail;
-     //   System.out.println("oldTail: " + oldTail);
-        final Node<Task> newNode = new Node<Task>(oldTail, task,null);
-    //    System.out.println("newNode: " + newNode);
+        if (historyNodesListLink.containsKey(task.taskId)){
+            removeNode(historyNodesListLink.get(task.taskId));
+        }
+
+        final Node<Task> oldTail = tail;
+        //   System.out.println("oldTail: " + oldTail);
+        final Node<Task> newNode = new Node<Task>(oldTail, task, null);
+        //    System.out.println("newNode: " + newNode);
         tail = newNode;
-     //   System.out.println("tail: " + newNode);
-
-
+        //   System.out.println("tail: " + newNode);
         if (oldTail == null) {
             head = newNode;
-    //        System.out.println("head: " + head);
+            //        System.out.println("head: " + head);
         } else {
             oldTail.next = newNode;
-       //     System.out.println("oldTail.next: " + oldTail.next);
+            //     System.out.println("oldTail.next: " + oldTail.next);
         }
-       nodesListLink.put(task.taskId, newNode);
-        System.out.println("task.taskId: " + task.taskId+ "  node.prev: " + newNode.prev +"  node.data: "
-                + newNode.data +"  node.next: " + newNode.next );
-        System.out.println("nodesListLink: " + nodesListLink);
+        historyNodesListLink.put(task.taskId, newNode);
+        System.out.println("task.taskId: " + task.taskId + "  node.prev: " + newNode.prev + "  node.data: "
+                + newNode.data + "  node.next: " + newNode.next);
+        System.out.println("nodesListLink: " + historyNodesListLink);
+
+    }
+public List<Task> getTasks(){
+        historyListForPrint = new LinkedList<>();
+        Node<Task> node = head;
+        while (node != null) {
+            historyListForPrint.add(node.data);
+            node = node.next;
+        }
+        for (int i = historyListForPrint.size() - 1; i >= 0; i--) {
+            System.out.println("запрос " + (i + 1) + ": " + historyListForPrint.get(i));
+        }
+        return historyListForPrint;
+    }
+
+    public void removeNode (Node<Task> node) {
 
     }
 
--- src/Main.java
+++ src/Main.java
@@ -2,66 +2,70 @@ public static void main(String[] args) {
 
     //Проверка по ТЗ спринта 4,5
 
-     TaskManager manager = Managers.getDefault();
+    TaskManager manager = Managers.getDefault();
     //создаем 2 задачи
     Task task1 = new Task("taskname1", "taskdescr1", TaskStatus.NEW);
-     manager.addTask(task1);
-     Task task2 = new Task("taskname2", "taskdescr2", TaskStatus.NEW);
-     manager.addTask(task2);
+    manager.addTask(task1);
+    Task task2 = new Task("taskname2", "taskdescr2", TaskStatus.NEW);
+    manager.addTask(task2);
     //Task task3 = new Task("taskname3", "taskdescr3", TaskStatus.NEW);
     //manager.addTask(task3);
-     //System.out.println("pppppppp"+manager.getTasks());
+    //System.out.println("pppppppp"+manager.getTasks());
     //создаем 2 эпика
     Epic epic1 = new Epic("epicname1", "epicdescr1", TaskStatus.NEW);
     manager.addEpic(epic1);
     Epic epic2 = new Epic("epicname2", "epicdescr2", TaskStatus.NEW);
-   manager.addEpic(epic2);
+    manager.addEpic(epic2);
     //создаем одну подзадачу в epicname1
     Subtask subtask1 = new Subtask("subtaskname1", "subtaskdescr1", 3, TaskStatus.NEW);
-  manager.addSubtask(subtask1);
+    manager.addSubtask(subtask1);
     //создаем 2 подзадачи в epicname2
-   Subtask subtask2 = new Subtask("subtaskname2", "subtaskdescr2", 3, TaskStatus.NEW);
-  manager.addSubtask(subtask2);
-  Subtask subtask3 = new Subtask("subtaskname3", "subtaskdescr3", 3, TaskStatus.NEW);
-  manager.addSubtask(subtask3);
+    Subtask subtask2 = new Subtask("subtaskname2", "subtaskdescr2", 3, TaskStatus.NEW);
+    manager.addSubtask(subtask2);
+    Subtask subtask3 = new Subtask("subtaskname3", "subtaskdescr3", 3, TaskStatus.NEW);
+    manager.addSubtask(subtask3);
     // проверка правильности работы кода
-     manager.printAllTasks(); // напечатать все задачи
+    manager.printAllTasks(); // напечатать все задачи
     //manager.deleteAllTasks(); //удалить все задачи
     System.out.println("Запросили по ид 1 и вторую задачи");
     manager.getTaskById(1); // получить данные по задаче по номеру ID
-   manager.getTaskById(2);
- //  manager.getTaskById(3);
-   manager.getEpicById(3);
-//    manager.getEpicById(4);
-  //  System.out.println(manager.getTaskById(1)); // получить данные по задаче по номеру ID
-   // System.out.println(manager.getTaskById(2));
+    manager.getTaskById(2);
+    //  manager.getTaskById(3);
+    manager.getEpicById(3);
+    manager.getEpicById(4);
+    //  System.out.println(manager.getTaskById(1)); // получить данные по задаче по номеру ID
+    // System.out.println(manager.getTaskById(2));
     System.out.println("смотрим историю");
     manager.getHistory();
     System.out.println("Обновляем задачи (меняем статус 1 и 2 на ин-прогресс и запрашиваем их");
-    manager.updateTask(new Task("taskname1", "taskdescr1", 1,TaskStatus.IN_PROGRESS));
-    manager.updateTask(new Task("taskname2", "taskdescr2", 2,TaskStatus.IN_PROGRESS));
-   // System.out.println(manager.getTaskById(1)); // получить данные по задаче по номеру ID
-   // System.out.println(manager.getTaskById(2));
+    manager.updateTask(new Task("taskname1", "taskdescr1", 1, TaskStatus.IN_PROGRESS));
+    manager.updateTask(new Task("taskname2", "taskdescr2", 2, TaskStatus.IN_PROGRESS));
+    // System.out.println(manager.getTaskById(1)); // получить данные по задаче по номеру ID
+    // System.out.println(manager.getTaskById(2));
     manager.getTaskById(1); // получить данные по задаче по номеру ID
     manager.getTaskById(2);
-    System.out.println(InMemoryHistoryManager.getNodesListLink());
+    manager.getSubtaskByID(5);
+    manager.getSubtaskByID(6);
+    manager.getSubtaskByID(7);
+
+   // System.out.println(InMemoryHistoryManager.getNodesListLink());
     System.out.println("смотрим историю");
     manager.getHistory();
     System.out.println("удаляем 2 задачу");
-   // manager.deleteTaskById(2);
+    // manager.deleteTaskById(2);
     System.out.println("смотрим историю");
-   // manager.getHistory();
-   // System.out.println(manager.getTaskById(1)); // получить данные по задаче по номеру ID
-   // System.out.println(manager.getTaskById(2));
-   // manager.deleteTaskById(2); //удалить задачу (передаем ID)
-   // manager.getHistory();
+    // manager.getHistory();
+    // System.out.println(manager.getTaskById(1)); // получить данные по задаче по номеру ID
+    // System.out.println(manager.getTaskById(2));
+    // manager.deleteTaskById(2); //удалить задачу (передаем ID)
+    // manager.getHistory();
     //System.out.println(manager.getTaskById(3));// manager.getHistory();
-   //  System.out.println(manager.getSubtaskByID(5)); // получить данные по подзадаче по номеру ID
+    //  System.out.println(manager.getSubtaskByID(5)); // получить данные по подзадаче по номеру ID
     // System.out.println(manager.getEpicById(3)); // получить данные по эпику по номеру ID
     //manager.deleteTaskById(2); //удалить задачу (передаем ID)
     //manager.deleteSubtaskById(5); //удалить задачу (передаем ID)
     //manager.deleteEpicById(6); //удалить задачу (передаем ID)
-   // manager.updateTask(new Task("taskname2", "taskdescr2", 2,TaskStatus.IN_PROGRESS)); //поменять статус задачи (передаем ID и новый статус)
+    // manager.updateTask(new Task("taskname2", "taskdescr2", 2,TaskStatus.IN_PROGRESS)); //поменять статус задачи (передаем ID и новый статус)
     //manager.updateSubtask(new Subtask("subtaskname2new", "subtaskdescr2new",6, 3, TaskStatus.DONE)); //поменять статус задачи (передаем ID и новый статус)
     //manager.updateSubtask(new Subtask("subtaskname3new", "subtaskdescr3new",7, 3, TaskStatus.DONE)); //поменять с
     // manager.updateEpic(new Epic("epicname1new", "epicdescr1new", 4, TaskStatus.NEW));
@@ -69,5 +73,5 @@ public static void main(String[] args) {
     // manager.updateSubtask(new Subtask("subtaskname3", "subtaskdescr3", 4, TaskStatus.DONE));
     //manager.updateEpic(new Epic("epicname1new", "epicdescr1new", 4, TaskStatus.NEW));
     //manager.changeTaskStatus(7,TaskStatus.DONE); //поменять статус задачи (передаем ID и новый статус)
-   // manager.getHistory();
+    // manager.getHistory();
 }
