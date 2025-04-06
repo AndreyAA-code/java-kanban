b00f944f29adc2ca34d759350a65b6bc83a8b79b
AndreyAA-code <otd53@yandex.ru>
2025-04-06 14:17:44 +0300

    реализовать тесты отправить на проверку

--- Test/InMemoryHistoryManagerTest.java
+++ Test/InMemoryHistoryManagerTest.java
@@ -2,6 +2,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.LinkedList;
 import java.util.List;
 
 class InMemoryHistoryManagerTest {
@@ -18,5 +19,38 @@ class InMemoryHistoryManagerTest {
         final List<Task> history = historyManager.getHistory();
         assertNotNull(history, "История не пустая.");
         assertEquals(1, history.size(), "История не пустая.");
+    }
+
+    @Test
+    void ifSameIdTaskremovedFromListWhileAdd () {
+
+        TaskManager manager = Managers.getDefault();
+        HistoryManager historyManager = Managers.getDefaultHistory();
+        Task task1 = new Task("taskname1", "taskdescr1", TaskStatus.NEW);
+        manager.addTask(task1);
+        manager.getTaskById(1); // получить данные по задаче по номеру ID
+        manager.getTaskById(1);
+        manager.getHistory();
+        List<Task> history = manager.getHistory();
+        System.out.println(history);
+        assertEquals(1, history.size(), "Объект не один");
+    }
+
+    @Test
+    void ifHistoryNullwhileDeleteAllTasks () {
+
+        TaskManager manager = Managers.getDefault();
+        HistoryManager historyManager = Managers.getDefaultHistory();
+        Task task1 = new Task("taskname1", "taskdescr1", TaskStatus.NEW);
+        manager.addTask(task1);
+        Task task2 = new Task("taskname2", "taskdescr2", TaskStatus.NEW);
+        manager.addTask(task2);
+        manager.getTaskById(1); // получить данные по задаче по номеру ID
+        manager.getTaskById(2);
+        manager.getHistory();
+        manager.deleteAllTasks();
+        List<Task> history = manager.getHistory();
+        System.out.println(history);
+        assertEquals(0, history.size(), "List не пустой");
     }
 }
