7b9d8d944996dab8b782b0ef27bb7ea1c3c38035
AndreyAA-code <otd53@yandex.ru>
2025-04-06 16:11:13 +0300

    убрать замечания Code Analysis отправить на проверку

--- src/Node.java
+++ src/Node.java
@@ -1,13 +1,13 @@
 public class Node<Task> {
 
-        public Task data;
-        public Node<Task> next;
-        public Node<Task> prev;
+    public Task data;
+    public Node<Task> next;
+    public Node<Task> prev;
 
-        public Node (Node<Task> prev, Task data, Node<Task> next) {
-            this.data = data;
-            this.next = next;
-            this.prev= prev;
-        }
+    public Node(Node<Task> prev, Task data, Node<Task> next) {
+        this.data = data;
+        this.next = next;
+        this.prev = prev;
+    }
 
 }
