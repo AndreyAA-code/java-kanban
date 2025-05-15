928188ae3aee22001dec004a3b261f218229b9f3
AndreyAA-code <otd53@yandex.ru>
2025-05-16 00:13:08 +0300

    http classes added

--- src/HttpTaskServer.java
+++ src/HttpTaskServer.java
@@ -4,6 +4,9 @@ import com.sun.net.httpserver.HttpServer;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.net.InetSocketAddress;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Optional;
 
 public class HttpTaskServer {
@@ -12,6 +15,7 @@ public class HttpTaskServer {
         int port = 8080;
 
         HttpServer httpServer = HttpServer.create(new InetSocketAddress(port),0);
+
         httpServer.createContext("/tasks", new TasksHandler());
     //    httpServer.createContext("/subtasks", new SubtasksHandler());
       //  httpServer.createContext("/epics", new EpicsHandler());
@@ -25,77 +29,47 @@ public class HttpTaskServer {
 }
 
 class TasksHandler implements HttpHandler {
-
-String basePath = "/tasks";
     @Override
     public void handle(HttpExchange httpExchange) throws IOException {
+
+        Path path = Paths.get("kanbanSave.csv");
+        if (!Files.exists(path)) {
+            Files.createFile(path);
+        }
+        //создаем новый менеджер с сохранением и передаем файл
+        TaskManager manager = FileBackedTaskManager.loadFromFile(path);
+
         System.out.println("Received tasks request");
         String method = httpExchange.getRequestMethod();
-        String path = httpExchange.getRequestURI().getPath();
-        String param = getPath(httpExchange,basePath);
+        String path1 = httpExchange.getRequestURI().getPath();
+        String[] pathArray = httpExchange.getRequestURI().getPath().split("/");
 
-        if (method.equals("GET")) {
-            if (param.isEmpty()) {
-                httpExchange.sendResponseHeaders(200, 0);
-                try (OutputStream os = httpExchange.getResponseBody()) {
-                    os.write((method + " пустой ").getBytes());
-                }
-                System.out.println(method + " пустой " + path);
-            } else {
-                httpExchange.sendResponseHeaders(200, 0);
-                Integer id = Integer.parseInt(param);
-                try (OutputStream os = httpExchange.getResponseBody()) {
-                    os.write((method + " id " + id ).getBytes());
-                }
-                System.out.println(method + " id " + path);
+        if (method.equals("GET") && pathArray.length == 2) {
+
+            httpExchange.sendResponseHeaders(200, 0);
+            try (OutputStream os = httpExchange.getResponseBody()) {
+                os.write((manager.getTasks()).toString().getBytes());
             }
-            return;
 
+        } else if (method.equals("GET") && pathArray.length == 3) {
+            httpExchange.sendResponseHeaders(202, 0);
+            Integer id = Integer.parseInt(pathArray[2]);
+            try (OutputStream os = httpExchange.getResponseBody()) {
+                os.write((manager.getTaskById(id)).toString().getBytes());
+            }
+            System.out.println(method + " id " + path1);
         } else if (method.equals("POST")) {
-            if (param.isEmpty()) {
-                httpExchange.sendResponseHeaders(200, 0);
-                try (OutputStream os = httpExchange.getResponseBody()) {
-                    os.write((method + " пустой " + path).getBytes());
-                }
-                System.out.println(method + " пустой " + path);
-            } else {
-                httpExchange.sendResponseHeaders(404, 0);
-                try (OutputStream os = httpExchange.getResponseBody()) {
-                    os.write(("ошибка").getBytes());
-                }
-                System.out.println(method + " id " + path);
-                return;
-            }
-        }  else if (method.equals("DELETE")) {
-                if (param.isEmpty()) {
-                    try (OutputStream os = httpExchange.getResponseBody()) {
-                        os.write((method + " пустой " + path).getBytes());
-                    }
-                    System.out.println(method + " пустой " + path);
-                } else {
-                    httpExchange.sendResponseHeaders(404, 0);
-                    try (OutputStream os = httpExchange.getResponseBody()) {
-                        os.write(("ошибка").getBytes());
-                    }
-                    System.out.println(method + " id " + path);
-                    return;
-                }
+
+
+        } else if (method.equals("DELETE")) {
+
         } else {
-            System.out.println("Unknown method and path " + method + " " + path);
+            System.out.println("Unknown method and path " + method + " " + path1);
         }
 
 
     }
-    public String getPath(HttpExchange httpExchange, String basePath) {
-        String param;
-        String path = httpExchange.getRequestURI().getPath();
-        if (path.contains("/basePath/")) {
-            param = path.substring(basePath.length() + 1);
-        }else{
-            param = path.substring(basePath.length());
-        }
-        return param;
-    }
+
 }
 
         /* String method = httpExchange.getRequestMethod();
