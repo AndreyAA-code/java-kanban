77993c934cbcd3868e5627e10fd4c5d8faf237e8
AndreyAA-code <otd53@yandex.ru>
2025-05-22 00:30:17 +0300

    add history handler add prioritised handler add 406 code for update check epic add

--- test/HttpServerTest.java
+++ test/HttpServerTest.java
@@ -6,7 +6,6 @@ import com.sun.net.httpserver.HttpServer;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.io.IOException;
@@ -17,8 +16,7 @@ import java.time.Duration;
 import java.time.LocalDateTime;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class HttpServerTest {
     final TaskManager manager = new InMemoryTaskManager();
@@ -28,11 +26,12 @@ public class HttpServerTest {
             .registerTypeAdapter(Duration.class, new DurationAdapter())
             .registerTypeAdapter(LocalDateTime.class, new LocalDateTimeAdapter())
             .create();
+    HttpServer httpServer;
 
     @BeforeEach
     public void SetUpServer() throws IOException, InterruptedException {
         int port = 8080;
-        HttpServer httpServer = HttpServer.create(new InetSocketAddress(port), 0);
+        httpServer = HttpServer.create(new InetSocketAddress(port), 0);
 
         httpServer.createContext("/tasks", new TasksHandler(manager));
         httpServer.createContext("/subtasks", new SubtasksHandler(manager));
@@ -42,15 +41,21 @@ public class HttpServerTest {
 
         httpServer.start();
         System.out.println("Local Server started on port: " + port);
+
     }
+    @AfterEach
+    public void TearDownServer() {
+        manager.deleteAllTasks();
+        httpServer.stop(1);
+        System.out.println("Local Server stopped");
+    }
+
     @Test
-    public void testAddTask() throws IOException, InterruptedException {
+    public void testTasksEndpoint() throws IOException, InterruptedException {
         // создаём задачу
         Task task = new Task("Test 2", "Testing task 2",
                 TaskStatus.NEW, Duration.ofMinutes(5), LocalDateTime.now());
-       // TaskManager manager = new InMemoryTaskManager();
         // конвертируем её в JSON
-
         String taskJson = gson.toJson(task);
 
         // создаём HTTP-клиент и запрос
@@ -70,5 +75,40 @@ public class HttpServerTest {
         assertNotNull(tasksFromManager, "Задачи не возвращаются");
         assertEquals(1, tasksFromManager.size(), "Некорректное количество задач");
         assertEquals("Test 2", tasksFromManager.get(0).taskName, "Некорректное имя задачи");
+
+        URI url1 = URI.create("http://localhost:8080/tasks/1");
+        HttpRequest request1 = HttpRequest.newBuilder().uri(url1).DELETE().build();
+        HttpResponse<String> response1 = client.send(request1, HttpResponse.BodyHandlers.ofString());
+        // проверяем код ответа
+        assertEquals(201, response.statusCode());
+        List<Task> tasksFromManager1 = manager.getTasks();
+
+        assertEquals(0, tasksFromManager.size(), "Некорректное количество задач");
+    }
+
+    @Test
+    public void testEpicsEndpoint() throws IOException, InterruptedException {
+        // создаём задачу
+        Epic epic1 = new Epic("epicname1", "epicdescr1", TaskStatus.NEW);
+        // конвертируем её в JSON
+        String taskJson1 = gson.toJson(epic1);
+
+        // создаём HTTP-клиент и запрос
+        HttpClient client = HttpClient.newHttpClient();
+        URI url = URI.create("http://localhost:8080/epics");
+        HttpRequest request = HttpRequest.newBuilder().uri(url).POST(HttpRequest.BodyPublishers.ofString(taskJson1)).build();
+
+        // вызываем рест, отвечающий за создание задач
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        // проверяем код ответа
+        assertEquals(201, response.statusCode());
+
+        // проверяем, что создалась одна задача с корректным именем
+
+        List<Epic> epicsFromManager = manager.getEpics();
+
+        assertNotNull(epicsFromManager, "Задачи не возвращаются");
+        assertEquals(1, epicsFromManager.size(), "Некорректное количество задач");
+        assertEquals("epicname1", epicsFromManager.get(0).taskName, "Некорректное имя задачи");
     }
 }
