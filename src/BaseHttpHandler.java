14e73ecf3cb2e9b7f2b500f5db4dbc62e08b75b1
AndreyAA-code <otd53@yandex.ru>
2025-05-22 13:35:00 +0300

    handler classes added 404 error added epics handler added http task ready add checks for path minor errors checked http server test created finish finish git check

--- src/BaseHttpHandler.java
+++ src/BaseHttpHandler.java
@@ -1,16 +1,11 @@
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
-import com.google.gson.TypeAdapter;
-import com.google.gson.reflect.TypeToken;
-import com.google.gson.stream.JsonReader;
-import com.google.gson.stream.JsonWriter;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.time.Duration;
 import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
 
 public class BaseHttpHandler implements HttpHandler {
     String method;
